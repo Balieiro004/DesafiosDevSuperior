91ab11dfa0b81d00de4bd5c3353f0ce06eec866c
Balieiro004 <Balieiro004@gmail.com>
2024-11-16 16:03:07 -0300

    Criaçao do projeto Componentes e Injeçao

---
+++ DesafiosDevSuperior/desafioComponentesInjecao/src/main/java/com/balieiro/desafioComponentesInjecao/DesafioComponentesInjecaoApplication.java
@@ -0,0 +1,13 @@
+package com.balieiro.desafioComponentesInjecao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioComponentesInjecaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioComponentesInjecaoApplication.class, args);
+	}
+
+}
