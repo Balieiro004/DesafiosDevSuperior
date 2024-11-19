ea0c11fa6c0615c07b08e0e0f0037322976346ec
Balieiro004 <Balieiro004@gmail.com>
2024-11-19 14:35:37 -0300

    Desafio 2 sistema de Evento

---
+++ SistemaEvento/src/test/java/com/balieiro/sistemaEvento/SistemaEventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balieiro.sistemaEvento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
